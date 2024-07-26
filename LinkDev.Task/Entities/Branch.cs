9c0d6888058f6c47eb22c2bfc347399632eb533d
ArmiaFekry <afekry@youxel.com>
2024-07-26 17:33:15 +0300

    Add project files.

---
+++ LinkDev.Task/Entities/Branch.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Recruiting_Company_Web_API.Entities
+{
+    public class Branch
+    {
+            [Key]
+            public Guid BranchId { get; set; }
+
+            [Required]
+            [Column(TypeName = "nvarchar(300)")]
+            public string Title { get; set; }
+
+            [Required]
+            [Column(TypeName = "time")]
+            public TimeSpan OpeningHour { get; set; }
+
+            [Required]
+            [Column(TypeName = "time")]
+            public TimeSpan ClosingHour { get; set; }
+
+            [Required]
+            [Column(TypeName = "nvarchar(300)")]
+            public string ManagerName { get; set; }
+
+
+    }
+}
